501fd5761cd80770729aac02bf9a21af342ff8d3
Irfaanpk <155081133+Irfaanpk@users.noreply.github.com>
2026-05-06 16:45:51 +0530

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect("mongodb://mongo:27017/tasks");
+
+const Task = mongoose.model("Task", {
+    title: String,
+    completed: Boolean
+});
+
+app.post("/tasks", async (req, res) => {
+    const task = new Task(req.body);
+    await task.save();
+    res.send(task);
+});
+
+app.get("/tasks", async (req, res) => {
+    const tasks = await Task.find();
+    res.send(tasks);
+});
+
+app.delete("/tasks/:id", async (req, res) => {
+    await Task.findByIdAndDelete(req.params.id);
+    res.send({ message: "Deleted" });
+});
+
+app.listen(5000, () => {
+    console.log("Backend running on port 5000");
+});
