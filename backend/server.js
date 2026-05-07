52804443b42becf2181783780dfc9d1724da4978
Irfaanpk <155081133+Irfaanpk@users.noreply.github.com>
2026-05-07 17:13:56 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,29 +7,49 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://mongo:27017/tasks");
+mongoose.connect("mongodb://mongo:27017/tasks")
+.then(() => {
+    console.log("MongoDB Connected");
+})
+.catch((err) => {
+    console.log(err);
+});
 
 const Task = mongoose.model("Task", {
     title: String,
     completed: Boolean
 });
 
+app.get("/", (req, res) => {
+    res.send("Backend Running 🚀");
+});
+
 app.post("/tasks", async (req, res) => {
+
     const task = new Task(req.body);
+
     await task.save();
+
     res.send(task);
 });
 
 app.get("/tasks", async (req, res) => {
+
     const tasks = await Task.find();
+
     res.send(tasks);
 });
 
 app.delete("/tasks/:id", async (req, res) => {
+
     await Task.findByIdAndDelete(req.params.id);
-    res.send({ message: "Deleted" });
+
+    res.send({
+        message: "Deleted"
+    });
 });
 
 app.listen(5000, () => {
+
     console.log("Backend running on port 5000");
 });
